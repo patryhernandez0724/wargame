68785e6c798e61975c0f2f93243b867036ff721f
Patricia Hernandez <patryhernandez0724@gmail.com>
2024-08-27 23:35:12 -0400

    firstDesign

--- War Card Game/ContentView.swift
+++ War Card Game/ContentView.swift
@@ -9,13 +9,42 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        
+        ZStack{
+            Color(.systemGreen)
+                .ignoresSafeArea()
+            VStack(spacing: 40.0) {
+                Image("logo")
+                HStack{
+                    Spacer()
+                    Image("card2")
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                }
+                Image("button")
+                HStack{
+                    Spacer()
+                    Text("Player")
+                    Spacer()
+                    Text("CPU")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("0")
+                    Spacer()
+                    Text("0")
+                    Spacer()
+                }
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+                    
+            }
+            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+            .foregroundColor(.white)
+                
         }
-        .padding()
+       
     }
 }
 
