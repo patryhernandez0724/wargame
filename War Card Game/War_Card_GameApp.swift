4ebeecfd94b631c8a913a13f8a3866d2b917d7c2
Patricia Hernandez <patryhernandez0724@gmail.com>
2024-08-27 23:11:33 -0400

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by Patricia on 8/27/24.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
